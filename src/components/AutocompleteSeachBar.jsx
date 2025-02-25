975525c1595a9142705d88ad9f78fd85f349db7a
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-02-25 23:28:55 +0530

    Enhance Autocomplete Search Bar with loading state and custom scrollbar styling

--- src/components/AutocompleteSeachBar.jsx
+++ src/components/AutocompleteSeachBar.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 const AutocompleteSeachBar = () => {
   const [searchedValue, setSearchedValue] = useState("");
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     setLoading(true);
@@ -33,15 +33,21 @@ const AutocompleteSeachBar = () => {
         className="rounded-lg border border-gray-300 p-2 w-[400px] outline-none hover:ring-4 transition-all duration-300 ease-in-out ring-blue-400"
         placeholder="Search..."
       />
-      {!loading && data?.length > 0
-        ? data?.map((item) => (
-            <p key={item?.id} className="text-sm">
-              {item?.name}
-            </p>
-          ))
-        : !loading && <p className="text-center mt-[30px]">No Data Found!</p>}
 
-      {loading && <p className="text-center mt-[30px]">Loading...</p>}
+      <div className="max-h-[300px] overflow-auto border border-gray-300 rounded-lg flex flex-col mt-0.5">
+        {!loading && data?.length > 0
+          ? data?.map((item) => (
+              <p
+                key={item?.id}
+                className="text-sm py-[5px] px-[10px] hover:bg-slate-100"
+              >
+                {item?.name}
+              </p>
+            ))
+          : !loading && <p className="text-center my-[30px]">No Data Found!</p>}
+
+        {loading && <p className="text-center my-[30px]">Loading...</p>}
+      </div>
     </div>
   );
 };
